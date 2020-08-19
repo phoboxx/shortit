690f27cc73b0820c6603d45715a025f27c9fab85
Frederic Legault <legault.f123@gmail.com>
2020-08-19 13:58:07 -0400

    Client - POC on react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/' component={ShortURL} />
+      </Switch>
+    </Router>
+  );
 }
+
+const Home = () => {
+  return (
+    <Fragment>
+      <h1>This is home</h1>
+    </Fragment>
+  );
+};
+
+const ShortURL = () => {
+  return (
+    <Fragment>
+      <h1>This is ShortURL</h1>
+    </Fragment>
+  );
+};
 
 export default App;
